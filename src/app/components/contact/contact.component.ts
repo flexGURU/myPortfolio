20ff5c494592d96a0918bae70e7e45d8ccba5031
John Nganga <mukunajohn329@gmail.com>
2024-12-14 12:50:23 +0300

    changes

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  form: FormGroup
+
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group{
+      
+    }
+
+  }
 
   goToUrl(url){
     window.open(url, "_blank")
